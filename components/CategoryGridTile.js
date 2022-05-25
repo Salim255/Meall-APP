48cc2023d7233f36779a9f00d4ca9bfb16a9b0d2
Salim <79255969+Salim255@users.noreply.github.com>
2022-05-25 15:14:40 +0200

    Grid

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { View, Pressable, Text, StyleSheet , Platform} from 'react-native'
+import { backgroundColor, shadowColor } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes'
+
+
+export default function CategoryGridTile({title, color}) {
+  return (
+    <View style={styles.gridItem}>
+        <Pressable android_ripple={{color: '#ccc'}} style={({ pressed }) =>  [styles.button, pressed ? styles.buttonPressed : null]}>
+            <View style={[styles.innerContainer, {backgroundColor: color}]}>
+                <Text style={styles.titile}>{title} </Text>
+            </View>
+        </Pressable>
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+    gridItem: {
+        flex: 1,
+        margin: 16,
+        height: 150,
+        borderRadius: 8,
+        elevation: 4,
+        backgroundColor: 'white',
+        shadowColor: 'black',
+        shadowOpacity: 0.25,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 8,
+        overflow: Platform.os === 'android' ? 'hidden': 'visible'
+    },
+    button:{
+        flex: 1,
+    },
+    buttonPressed: {
+        opacity: 0.5,
+
+    },
+    innerContainer:{
+        flex: 1,
+        padding: 16,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 8,
+    },
+    titile:{
+        fontWeight: 'bold',
+        fontSize:18,
+    }
+})
---
+++ screens/CategoriesSceen.js
@@ -0,0 +1,18 @@
+import { FlatList } from 'react-native';
+import CategoryGridTile from '../components/CategoryGridTile';
+import {CATEGORIES} from '../data/dummy-data';
+
+const rendderCategoryItm =(itemData) =>{
+    return <CategoryGridTile title={itemData.item.title} color={itemData.item.color}/>;
+}
+
+function CategoriesScreen () {
+    return <FlatList data={CATEGORIES}
+     keyExtractor={(item) => item.id} 
+     renderItem={rendderCategoryItm}
+     numColumns={2}
+     />
+
+}
+
+export default CategoriesScreen
