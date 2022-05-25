c971359884b206844cfa34ffbf3f0d745d6134cf
Salim <79255969+Salim255@users.noreply.github.com>
2022-05-25 15:49:14 +0200

    React-Native-Navigation

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import CategoriesScreen from './screens/CategoriesSceen';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();//Object with two props
 
 export default function App() {
   return (
        <> 
-          <StatusBar style='light'/>
-          <CategoriesScreen/>
+          <StatusBar style='dark'/>
+          <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="MealsCategories" component={CategoriesScreen}/>
+            </Stack.Navigator>
+            
+          </NavigationContainer>
        </>
   );
 }
