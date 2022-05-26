cf6e8a462c607b4ba25ee07a8fbaca8b76295574
Salim <79255969+Salim255@users.noreply.github.com>
2022-05-26 10:06:57 +0200

    Displaying Meals

---
+++ components/MealItem.js
@@ -0,0 +1,9 @@
+import {View , Text} from 'react-native';
+
+export const MealItem = ({title}) => {
+  return (
+    <View>
+        <Text>{ title }</Text>
+    </View>
+  )
+}
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,14 +1,29 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, FlatList, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 import {MEALS} from '../data/dummy-data';
+import { MealItem } from '../components/MealItem';
 
 
 export const MealsOverviewScreen = ({ route }) => {
+   //const route = useRoute();
     //We get the navigation  and route because the component is registred as screen in app.js
    const catId =  route.params.categoryId;
-   //This how to get the gategoryId that passed throuth navigation on the screen 
+   //This how to get the gategoryId that passed throuth navigation on the screen
+   
+   const displyedMeals = MEALS.filter((mealItem) => {
+       return mealItem.categoryIds.indexOf(catId) >= 0;
+   });
+
+   const  renderMealItem = (itemData) =>{
+            return <MealItem title={ itemData.item.title }></MealItem>
+   }
+
+
   return (
     <View style={styles.container}>
-       <Text>Meals Overview Screen - {catId}</Text>
+        <FlatList data={ displyedMeals } keyExtractor={(item) => item.id} 
+        renderItem={renderMealItem}
+        />
     </View>
   )
 }
